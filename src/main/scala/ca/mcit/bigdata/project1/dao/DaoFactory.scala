a7428b7200b9eee986afc1a963f2ece3cd3e265e
David Parr <dvdprr6@gmail.com>
2018-08-04 21:16:46 -0400

    cleanup

--- src/main/scala/ca/mcit/bigdata/project1/dao/DaoFactory.scala
+++ src/main/scala/ca/mcit/bigdata/project1/dao/DaoFactory.scala
@@ -3,7 +3,7 @@ package ca.mcit.bigdata.project1.dao
 import ca.mcit.bigdata.project1.model.Model
 
 object DaoFactory {
-  def getDao[ DAO <: Dao[_ <: Model]](daoClass: Class[DAO]): DAO = {
+  def getDao[DAO <: Dao[_ <: Model]](daoClass: Class[DAO]): DAO = {
     val dao: DAO = daoClass.newInstance()
     return dao
   }
