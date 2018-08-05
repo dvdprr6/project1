d527d27e90b145c6679cd5eacd46341ed69aec8d
David Parr <dvdprr6@gmail.com>
2018-08-04 21:12:24 -0400

    not needed

--- src/main/scala/ca/mcit/bigdata/project1/parser/FileReaderParser.scala
+++
@@ -1,9 +0,0 @@
-package ca.mcit.bigdata.project1.parser
-
-import ca.mcit.bigdata.project1.model.{CalenderDate, Route, Trip}
-
-trait FileReaderParser {
-  def parseTrip(subLine: String): Trip
-  def parseRoute(subLine: String): Route
-  def parseCalenderDate(subLine: String): CalenderDate
-}
